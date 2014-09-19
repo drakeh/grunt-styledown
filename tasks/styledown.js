5706cd5f5303471b994620b33ce92f943817ee4e
Drake Hampton <drakehamp@gmail.com>
2014-09-18 21:49:51 -0500

    Yeoman scaffold

---
+++ tasks/styledown.js
@@ -0,0 +1,51 @@
+/*
+ * grunt-styledown
+ * https://github.com/drakeh/grunt-styledown
+ *
+ * Copyright (c) 2014 Drake Hampton
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('styledown', 'Grunt plugin to generate style guides via styledown', function () {
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (file) {
+            // Concat specified files.
+            var src = file.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
+
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(file.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + file.dest + '" created.');
+        });
+    });
+
+};
